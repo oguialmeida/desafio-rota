4b9868367751c631d032ec665f237ef82c165615
Guilherme Gomes <gui.rock08@gmail.com>
2023-06-07 23:45:04 -0300

    Refatorando divisor

--- Divisor_de_conta/divisor_de_conta.js
+++ Divisor_de_conta/divisor_de_conta.js
@@ -1,27 +1,74 @@
-/* 
-A sua tarefa será criar uma calculadora capaz de dividir a conta de um estabelecimento
-para cada cliente considerando somente o consumido por cada um. Considerar que a
-calculadora somente faz a divisão de uma conta por vez.
-- Deve ser possível inserir múltiplos produtos e clientes;
-- Um cliente pode ter consumido vários, um ou nenhum produto;
-- Um produto pode ter sido consumido por vários ou um cliente;
-- Deve ser verificado a inclusão dos 10% do serviço para cada pessoa individualmente,
-sendo a taxa aplicada ao valor que cada um gastou.
-*/
-
 let contador = 0;
+let itensPorCliente = {};
 
 function adicionarItem() {
-    let novoItem = document.createElement("li")
-    contador++
-    novoItem.innerText = "Cliente " + contador + " "
-    let botaoRemover = document.createElement("button")
-    botaoRemover.innerText = "Remover"
-    botaoRemover.onclick = function() { removerItem(botaoRemover.parentNode) }
-    novoItem.appendChild(botaoRemover)
-    document.getElementById("lista").appendChild(novoItem)
+  let item = document.getElementById("inputItem").value;
+  let preco = parseFloat(document.getElementById("inputPreco").value);
+  let cliente = document.getElementById("inputCliente").value;
+
+  if (item.trim() === "" || preco <= 0 || cliente.trim() === "") {
+    alert("Por favor, preencha corretamente o item, o preço e o cliente.");
+    return;
+  }
+
+  if (!itensPorCliente.hasOwnProperty(cliente)) {
+    itensPorCliente[cliente] = [];
+  }
+
+  itensPorCliente[cliente].push(preco);
+
+  let novoItem = document.createElement("li");
+  contador++;
+  novoItem.classList.add("list-group-item");
+  novoItem.innerHTML =
+    "Pedido " +
+    contador +
+    ": " +
+    item +
+    " (Preço: R$" +
+    preco.toFixed(2) +
+    ", Cliente: " +
+    cliente +
+    ")";
+  let botaoRemover = document.createElement("button");
+  botaoRemover.innerText = "Remover";
+  botaoRemover.classList.add("btn", "btn-danger", "btn-sm", "ml-2");
+  botaoRemover.onclick = function () {
+    removerItem(botaoRemover.parentNode, cliente, preco);
+  };
+  novoItem.appendChild(botaoRemover);
+  document.getElementById("lista").appendChild(novoItem);
+  document.getElementById("inputItem").value = "";
+  document.getElementById("inputPreco").value = "";
+  document.getElementById("inputCliente").value = "";
 }
 
-function removerItem(item) {
-    item.parentNode.removeChild(item)
+function removerItem(item, cliente, precoItem) {
+  item.parentNode.removeChild(item);
+  if (itensPorCliente.hasOwnProperty(cliente)) {
+    const index = itensPorCliente[cliente].indexOf(precoItem);
+    if (index > -1) {
+      itensPorCliente[cliente].splice(index, 1);
+    }
+  }
+}
+
+function calcularTotal() {
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = "";
+
+  for (let cliente in itensPorCliente) {
+    if (itensPorCliente.hasOwnProperty(cliente)) {
+      let total = itensPorCliente[cliente].reduce((acc, curr) => acc + curr, 0);
+      let totalComTaxa = total * 1.1;
+      resultado.innerHTML +=
+        "Cliente " +
+        cliente +
+        ": R$" +
+        total.toFixed(2) +
+        " + 10% = R$" +
+        totalComTaxa.toFixed(2) +
+        "<br>";
+    }
+  }
 }
